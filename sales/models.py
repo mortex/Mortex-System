25010709fd26eda3f94462f42274536e715083ba
Eric Michel <ytfeldrawkcab@gmail.com>
2011-02-20 17:12:44 -0500

    Modified models

--- sales/models.py
+++ sales/models.py
@@ -1,51 +1,78 @@
 from django.db import models
 
-class ShirtStyles(models.Model):
+class ShirtStyle(models.Model):
 	ShirtStyleNumber = models.CharField('Style Number', max_length=20)
 	ShirtStyleDescription = models.CharField('Description', max_length=200)
+	Customer = models.ForeignKey(Customer, null=True)
+	KnitStyleName = models.CharField('Knit Style', max_length=10)
 	def __unicode__(self):
         	return self.ShirtStyleNumber + ' ' + self.ShirtStyleDescription	
 
-class StyleColorCategories(models.Model):
+class StyleColorCategory(models.Model):
 	StyleColorCategoryName = models.CharField('Color Category', max_length=20)
+	def __unicode__(self):
+		return self.StyleColorCategoryName
 
-class ShirtStyleSKUs(models.Model):
-	ShirtStyle = models.ForeignKey(ShirtStyles)
-	StyleColorCategory = models.ForeignKey(StyleColorCategories)
-	ShirtStyleSize = models.CharField('Size', max_length=2)
+class StyleColor(models.Model):
+	StyleColorCategory = models.ForeignKey(StyleColorCategory)
+	StyleColorName = modelsCharField('Color Name', max_length=20)
+	def __unicode__(self):
+		return self.StyleColorName
+
+class ShirtStylePrice(models.Model):
+	ShirtStyle = models.ForeignKey(ShirtStyle)
+	StyleColorCategory = models.ForeignKey(StyleColorCategory)
+	ShirtStyleSize = models.CharField('Size', max_length=10)
 	FabricRollYield = models.IntegerField('Fabric Roll Yield')
 	KnitSize = models.FloatField('Knit Size')
 	SizePrice = models.FloatField('Size Price')
 	Active = models.BooleanField()
 	def __unicode__(self):
         	return self.ShirtStyleSize
+
+class ShirtStyleSKU(models.Model):
+	ShirtStylePrice = models.ForeignKey(ShirtStylePrice)
+	StyleColor = models.ForeignKey(StyleColor)
 	
-class Customers(models.Model):
+class Customer(models.Model):
 	CustomerName = models.CharField('Customer Name', max_length=40)
 	def __unicode__(self):
         	return self.CustomerName
 
-class ShirtOrders(models.Model):
-	Customer = models.ForeignKey(Customers)
+class CustomerAddress(models.Model):
+	Customer = models.ForeignKey(Customer)
+	Address1 = models.CharField('Address 1', max_length=40)
+	Address2 = models.Charfield('Address 2', max_length=40)
+	City = models.CharField(max_length=20)
+	State = models.CharField(max_length=2)
+	PostalCode = models.CharField(max_length=10)
+	ContactName = models.Charfield(max_length=40)
+
+class ShirtOrder(models.Model):
+	CustomerAddress = models.Foreign(CustomerAddress)
 	PONumber = models.CharField('PO#', max_length=20)
 	Complete = models.BooleanField()
 	def __unicode__(self):
         	return self.PONumber
 
-class ShirtOrderSKUs(models.Model):
-	ShirtOrder = models.ForeignKey(ShirtOrders)
-	ShirtStyleSKU = models.ForeignKey(ShirtStyleSKUs)
+class ShirtOrderSKU(models.Model):
+	ShirtOrder = models.ForeignKey(ShirtOrder)
+	ShirtStyleSKU = models.ForeignKey(ShirtStyleSKU)
 	OrderQuantity = models.IntegerField('Quantity')
-	
-class Shipments(models.Model):
-	DateShipped = models.DateTimeField('Date Shipped')
+	Price = models.FloatField()
+	CustomLabel = models.BooleanField('Custom Label')
 	
 class ShirtSKUInventory(models.Model):
+	ShirtStyleSKU = models.ForeignKey(ShirtStyleSKU)
 	CutOrder = models.CharField('Cut Order', max_length=20)
 	Pieces = models.IntegerField()
 	Add = models.BooleanField()
 
-class ShipmentSKUs(models.Model):
-	Shipment = models.ForeignKey(Shipments)
-	ShirtOrderSKU = models.ForeignKey(ShirtOrderSKUs)
+class Shipment(models.Model):
+	DateShipped = models.DateTimeField('Date Shipped')
+	TrackingNumber = models.CharField('Tracking Number', max_length=50)
+
+class ShipmentSKU(models.Model):
+	Shipment = models.ForeignKey(Shipment)
+	ShirtOrderSKU = models.ForeignKey(ShirtOrderSKU)
 	CutOrder = models.CharField('Cut Order', max_length=20)
