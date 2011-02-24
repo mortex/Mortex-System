fb46be3ea51a5f8698d92af51840a087ac996e52
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-02-24 18:30:13 -0500

    Just made a few notes in some of the files

--- sales/views.py
+++ sales/views.py
@@ -13,8 +13,8 @@ def manageinventory(request, object_id):
     shirtstyleinventory = ShirtStyleSKU.objects.annotate(total_inventory=Sum('shirtskuinventory__Pieces')).filter(total_inventory__gt=0)
     shirtstylecolors = StyleColor.objects.annotate().filter(shirtstylesku__ShirtStylePrice__ShirtStyle__id=object_id)
 
-    display_colors = {}
-    for color in set([sku.StyleColor for sku in shirtstyleinventory]):
+    display_colors = {} #empty dictionary
+    for color in set([sku.StyleColor for sku in shirtstyleinventory]): #conversion to 'set' eliminates duplicates
         display_colors[color] = [s for s in shirtstyleinventory if s.StyleColor == color]
 
     shirt_style = ShirtStyle.objects.get(pk=object_id)
