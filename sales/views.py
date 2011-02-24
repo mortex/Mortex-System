34bfae420d064606e107128d4d90f9c8263f2c2a
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-02-24 03:22:08 -0500

    Did more work on the inventory template and view
    Created an aggregate function to total up inventory transactions for a summary sheet.

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
-from sales.models import StyleColor, ShirtStyle, ShirtSKUInventory
+from sales.models import StyleColor, ShirtStyle, ShirtSKUInventory, ShirtStyleSKU
 from django.shortcuts import render_to_response
+from django.db.models import Sum
 
 def styleorder(request, object_id):
 	colors = StyleColor.objects.all()
@@ -8,6 +9,8 @@ def styleorder(request, object_id):
 	return render_to_response('sales/styleorder.html', {'colors': colors, 'shirtstyle': shirtstyle})
 
 def manageinventory(request, object_id):
-	ss = ShirtStyle.objects.get(pk=object_id)
-	skuinventory = ShirtSKUInventory.objects.filter(ShirtStyleSKU__ShirtStylePrice__ShirtStyle__exact=object_id)
-	return render_to_response('sales/manageinventory.html', {'skuinventory': skuinventory})
+	#ss = ShirtStyle.objects.get(pk=object_id)
+	#skuinventory = ShirtSKUInventory.objects.filter(ShirtStyleSKU__ShirtStylePrice__ShirtStyle__exact=object_id)
+	#return render_to_response('sales/manageinventory.html', {'skuinventory': skuinventory})
+	shirtstyleinventory = ShirtStyleSKU.objects.annotate(total_inventory=Sum('shirtskuinventory__Pieces')).filter(total_inventory__gt=0)
+	return render_to_response('sales/manageinventory.html', {'shirtstyleinventory': shirtstyleinventory})
