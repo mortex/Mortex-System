ddba82f17f687e4b1efa3a2820cccb36f95cc058
Taylor Hedberg <tmhedberg@gmail.com>
2011-02-24 14:43:07 -0500

    Display inventory as hierarchical list

--- sales/views.py
+++ sales/views.py
@@ -4,13 +4,16 @@ from django.shortcuts import render_to_response
 from django.db.models import Sum
 
 def styleorder(request, object_id):
-	colors = StyleColor.objects.all()
-	shirtstyle = ShirtStyle.objects.get(pk=object_id)
-	return render_to_response('sales/styleorder.html', {'colors': colors, 'shirtstyle': shirtstyle})
+    colors = StyleColor.objects.all()
+    shirtstyle = ShirtStyle.objects.get(pk=object_id)
+    return render_to_response('sales/styleorder.html', {'colors': colors, 'shirtstyle': shirtstyle})
 
 def manageinventory(request, object_id):
-	#ss = ShirtStyle.objects.get(pk=object_id)
-	#skuinventory = ShirtSKUInventory.objects.filter(ShirtStyleSKU__ShirtStylePrice__ShirtStyle__exact=object_id)
-	#return render_to_response('sales/manageinventory.html', {'skuinventory': skuinventory})
-	shirtstyleinventory = ShirtStyleSKU.objects.annotate(total_inventory=Sum('shirtskuinventory__Pieces')).filter(total_inventory__gt=0)
-	return render_to_response('sales/manageinventory.html', {'shirtstyleinventory': shirtstyleinventory})
+
+    shirtstyleinventory = ShirtStyleSKU.objects.annotate(total_inventory=Sum('shirtskuinventory__Pieces')).filter(total_inventory__gt=0)
+
+    display_colors = {}
+    for color in set([sku.StyleColor for sku in shirtstyleinventory]):
+        display_colors[color] = [s for s in shirtstyleinventory if s.StyleColor == color]
+
+    return render_to_response('sales/manageinventory.html', {'display_colors': display_colors})
