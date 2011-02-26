a0579d4907f6c724c0e052025e89e3a3e0a85f44
Taylor Hedberg <tmhedberg@gmail.com>
2011-02-26 18:34:54 -0500

    Add admin media path to URL dispatcher

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
@@ -13,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^admin-media/(?P<path>.*)$', 'django.views.static.serve', {"document_root": settings.MEDIA_ROOT}),
     (r'^app/', include('sales.urls')),
 )
