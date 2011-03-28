ccb4651e737d6b29c3b9f83af913f63293d27cec
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-03-27 20:13:02 -0400

    cleaned up views.py and urls.py for fresh start

--- sales/urls.py
+++ sales/urls.py
@@ -5,9 +5,4 @@ info_dict = {
     'queryset': ShirtStyle.objects.all(),
 }
 
-urlpatterns = patterns('',
-    (r'^styles/$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^styles/(?P<object_id>\d+)/$', 'sales.views.styleorder'),
-    (r'^inventory/$', 'django.views.generic.list_detail.object_list', info_dict),
-    (r'^inventory/(?P<object_id>\d+)/$', 'sales.views.manageinventory'),
-)
+urlpatterns = patterns('',)
