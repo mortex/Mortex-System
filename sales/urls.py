79e63bdca641f48c2a5d07f9616e0349b87a2d79
Taylor Hedberg <tmhedberg@gmail.com>
2011-04-09 22:07:00 -0400

    sales/urls.py: Expand tabs to spaces

--- sales/urls.py
+++ sales/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls.defaults import *
 from sales.models import ShirtStyle
 
 info_dict = {
-	'queryset': ShirtStyle.objects.all(),
+    'queryset': ShirtStyle.objects.all(),
 }
 
 urlpatterns = patterns('',
-	(r'^styles/$', 'django.views.generic.list_detail.object_list', info_dict),
-	(r'^styles/(?P<object_id>\d+)/$', 'sales.views.styleorder'),
-	(r'^inventory/$', 'django.views.generic.list_detail.object_list', info_dict),
-	(r'^inventory/(?P<object_id>\d+)/$', 'sales.views.manageinventory'),
+    (r'^styles/$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^styles/(?P<object_id>\d+)/$', 'sales.views.styleorder'),
+    (r'^inventory/$', 'django.views.generic.list_detail.object_list', info_dict),
+    (r'^inventory/(?P<object_id>\d+)/$', 'sales.views.manageinventory'),
 )
