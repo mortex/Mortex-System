d9dd28061e1ae1bcf7bc97898e869f603479a69d
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-04-10 14:25:29 -0400

    [WIP] shirt orders is fully working, view code added to process validation, csrf not working

--- sales/data_views.py
+++ sales/data_views.py
@@ -1,7 +1,8 @@
-from sales.models import CustomerAddress
+from sales.models import CustomerAddress, ShirtStyle, ShirtPrice, ShirtStyleVariation
 from django.core import serializers
 from django.http import HttpRequest, HttpResponse
 
+#this function allows us to traverse relationships if necessary
 def serialize(dictionary, relations=None):
     json_serializer = serializers.get_serializer("json")()
     response = HttpResponse(mimetype="application/json")
@@ -11,11 +12,6 @@ def serialize(dictionary, relations=None):
         json_serializer.serialize(dictionary, ensure_ascii=False, stream=response, relations=relations)
     
     return response
-
-def colors(request):
-    shirtstyleid = request.GET['shirtstyleid']
-    colors = Color.objects.distinct().filter(ColorCategory__shirtprice__ShirtStyle__id=shirtstyleid)
-    return serialize(colors)
     
 def styleprices(request):
     shirtstyleid = request.GET['shirtstyleid']
@@ -27,3 +23,26 @@ def customeraddresses(request):
     customerid = request.GET['customerid']
     addresses = CustomerAddress.objects.filter(Customer__exact=customerid)
     return serialize(addresses)
+
+def shirtstyles(request):
+    if "customerid" in request.GET:
+        customerid = request.GET['customerid']
+        shirtstyles = ShirtStyle.objects.filter(Customer__exact=customerid)
+    else:
+        shirtstyleid = request.GET['id']
+        shirtstyles = ShirtStyle.objects.filter(id=shirtstyleid) #must be filter and not get because JSON requires an array
+    
+    return serialize(shirtstyles)
+
+def shirtstylevariations(request):
+    if "customerid" in request.GET:
+        customerid = request.GET['customerid']
+        shirtstylevariations = ShirtStyleVariation.objects.filter(Customer__exact=customerid)
+        
+    else:
+        shirtstylevariationid = request.GET['id']
+        shirtstylevariations = ShirtStyleVariation.objects.filter(id=shirtstylevariationid)
+        if "getparent" in request.GET and request.GET['getparent'] == "true":
+            return serialize(shirtstylevariations, relations=('ShirtStyle',))
+
+    return serialize(shirtstylevariations)
--- sales/views.py
+++ sales/views.py
@@ -1,12 +1,55 @@
 # Create your views here.
-from sales.models import Color, ShirtStyle, ShirtOrder
+from sales.models import Color, ShirtStyle, ShirtOrder, ShirtStyleVariation
 from django.shortcuts import render_to_response
 from sales.forms import Order, OrderLine
+from django.template import RequestContext
 
 def shirtorders(request):
-    shirtorders = ShirtOrder.objects.all()
-    
-    return render_to_response('sales/shirtorders/list.html', {'shirtorders':shirtorders})
+    if request.method == 'GET':
+        shirtorders = ShirtOrder.objects.all()
+        my_context = RequestContext(request, {'shirtorders':shirtorders})
+        return render_to_response('sales/shirtorders/list.html', my_context)
+    else:
+        #created variable to test validation
+        passedvalidation = True
+        order = Order(request.POST)
+        
+        #if order is not valid, change validation variable
+        if not order.is_valid():
+            passedvalidation = False
+            
+        #created separate orderlines from post data
+        orderlines = []
+        for i in xrange(1, int(request.POST['rows']) + 1):
+            orderlines.append(OrderLine(request.POST, prefix=i, shirtstyleid=request.POST[str(i) + "-shirtstyleid"]))
+        
+        #check each orderline for validity, change validation variable if any of them fail
+        for orderline in orderlines:
+            if not orderline.is_valid():
+                passedvalidation = False
+        
+        #if any validation failed, send data back to page for revision
+        if passedvalidation == False:
+            #print orderlines[0]
+            my_context = {"orderlines": orderlines, "shirtorder": order}
+            return render_to_response("sales/shirtorders/add.html", my_context)
+        
+        #if all validation passed, save order, then save orderlines
+        else:
+            shirtorder = ShirtOrder(CustomerAddress=order.cleaned_data['CustomerAddress'], PONumber=order.cleaned_data['PONumber'])
+            shirtorder.save()
+            for orderline in orderlines:
+                for s in xrange(1, orderline.cleaned_data["sizes"]):
+                    if 'quantity'+str(s) in orderline.fields and orderline.cleaned_data['quantity'+str(s)] != None:
+                        ShirtOrderSKU( ShirtOrder=shirtorder
+                                     , ShirtPrice=ShirtPrice.objects.get(pk=orderline.cleaned_data['pricefkey'+str(s)])
+                                     , ShirtStyleVariation= None if orderline.cleaned_data['shirtstylevariation']==None else ShirtStyleVariation.objects.get(pk=orderline.cleaned_data['shirtstylevariation'])
+                                     , Color=orderline.cleaned_data['color']
+                                     , OrderQuantity=orderline.cleaned_data['quantity'+str(s)]
+
+                                     , Price=orderline.cleaned_data['price'+str(s)]
+                                     ).save()
+            return super(ShirtOrderAdmin, self).add_view(request, form_url="")
 
 def shirtorderview(request, orderid):
     shirtorder = ShirtOrder.objects.get(pk=orderid)
