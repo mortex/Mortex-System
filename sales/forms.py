13966cd8713cae1b147fdbd25780acacbecd6ce0
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-04-30 14:33:50 -0400

    finally resolved template issue with dynamically generated form - [WIP] fully populating form after validation error

--- sales/forms.py
+++ sales/forms.py
@@ -24,17 +24,23 @@ class OrderLine(forms.Form):
         self.fields["shirtstyleid"] = forms.IntegerField(widget=forms.HiddenInput(), initial=shirtstyleid)
         self.fields["color"] = forms.ModelChoiceField(queryset=Color.objects.distinct().filter(ColorCategory__shirtprice__ShirtStyle__id=shirtstyleid), widget=forms.Select(attrs={"onChange":"selectcolor(this.value, " + str(shirtstyleid) + ", " + str(self.prefix) + ")"}))
         shirtsizes = ShirtSize.objects.filter(shirtprice__ShirtStyle__exact=shirtstyleid).distinct()
-        self.fieldset = []
         i = 1
+
+        self.quantitylist = []
+        self.pricelist = []
+        self.pricefkeylist = []
 
         for size in shirtsizes:
             quantityid = 'quantity'+str(i)
             priceid = 'price'+str(i)
             pricefkeyid = 'pricefkey'+str(i)
-            self.fieldset.append((quantityid, priceid, pricefkeyid))
             self.fields[quantityid] = forms.IntegerField(label=size.ShirtSizeAbbr, min_value=0, required=False, widget=forms.TextInput(attrs={"disabled":None, "size":"6","class":"quantity size"+str(size.pk)}))
+            self.quantitylist.append(self[quantityid])
             self.fields[priceid] = forms.DecimalField(label=None, min_value=0, max_digits=8, decimal_places=2, required=False, widget=forms.TextInput(attrs={"size":"6","class":"price size"+str(size.pk)}))
+            self.pricelist.append(self[priceid])
             self.fields[pricefkeyid] = forms.IntegerField(label='fkey', required=False, widget=forms.HiddenInput(attrs={"class":"pricefkey size"+str(size.pk)}))
+            self.pricefkeylist.append(self[pricefkeyid])
             i+=1
 
+        self.fieldlist = zip(self.quantitylist, self.pricelist, self.pricefkeylist)
         self.fields["sizes"] = forms.IntegerField(widget=forms.HiddenInput(), initial=i-1)
