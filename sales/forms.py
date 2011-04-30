14d77e82346d29b0aa6f3cca717223b7444bdda8
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-04-30 18:42:20 -0400

    data completely populated in edit form, all javascript working with initial data

--- sales/forms.py
+++ sales/forms.py
@@ -13,6 +13,10 @@ class OrderLine(forms.Form):
 
         shirtstyleid = kwargs.pop("shirtstyleid")
         shirtstylevariationid = kwargs.pop("shirtstylevariationid", None)
+        colorid = kwargs.pop("colorid", None)
+        existingskus = kwargs.pop("existingskus", None)
+        
+        self.color = Color.objects.get(pk=colorid) if colorid else None
         if shirtstylevariationid != None:
             self.shirtstylevariation = ShirtStyleVariation.objects.get(pk=shirtstylevariationid)
         self.shirtstyle = ShirtStyle.objects.get(pk=shirtstyleid)
@@ -22,7 +26,7 @@ class OrderLine(forms.Form):
         self.fields["shirtstylevariationid"] = forms.IntegerField(widget=forms.HiddenInput(), required=False, initial=shirtstylevariationid)
 
         self.fields["shirtstyleid"] = forms.IntegerField(widget=forms.HiddenInput(), initial=shirtstyleid)
-        self.fields["color"] = forms.ModelChoiceField(queryset=Color.objects.distinct().filter(ColorCategory__shirtprice__ShirtStyle__id=shirtstyleid), widget=forms.Select(attrs={"class":"colorselector","onChange":"selectcolor(this.value, " + str(shirtstyleid) + ", " + str(self.prefix) + ")"}))
+        self.fields["color"] = forms.ModelChoiceField(queryset=Color.objects.distinct().filter(ColorCategory__shirtprice__ShirtStyle__id=shirtstyleid), initial=self.color, widget=forms.Select(attrs={"class":"colorselector","onChange":"selectcolor(this.value, " + str(shirtstyleid) + ", " + str(self.prefix) + ")"}))
         shirtsizes = ShirtSize.objects.filter(shirtprice__ShirtStyle__exact=shirtstyleid).distinct()
         i = 1
 
@@ -34,9 +38,18 @@ class OrderLine(forms.Form):
             quantityid = 'quantity'+str(i)
             priceid = 'price'+str(i)
             pricefkeyid = 'pricefkey'+str(i)
-            self.fields[quantityid] = forms.IntegerField(label=size.ShirtSizeAbbr, min_value=0, required=False, widget=forms.TextInput(attrs={"disabled":None, "size":"6","class":"quantity size"+str(size.pk)}))
+            
+            try:
+                sku = [sku for sku in existingskus if sku.ShirtPrice.ShirtSize == size][0]
+                skuquantity = sku.OrderQuantity
+                skuprice = sku.Price
+            except IndexError:
+                skuquantity = None
+                skuprice = None
+            
+            self.fields[quantityid] = forms.IntegerField(label=size.ShirtSizeAbbr, initial=skuquantity, min_value=0, required=False, widget=forms.TextInput(attrs={"disabled":None, "size":"6","class":"quantity size"+str(size.pk)}))
             self.quantitylist.append(self[quantityid])
-            self.fields[priceid] = forms.DecimalField(label=None, min_value=0, max_digits=8, decimal_places=2, required=False, widget=forms.TextInput(attrs={"size":"6","class":"price size"+str(size.pk)}))
+            self.fields[priceid] = forms.DecimalField(label=None, initial=skuprice, min_value=0, max_digits=8, decimal_places=2, required=False, widget=forms.TextInput(attrs={"size":"6","class":"price size"+str(size.pk)}))
             self.pricelist.append(self[priceid])
             self.fields[pricefkeyid] = forms.IntegerField(label='fkey', required=False, widget=forms.HiddenInput(attrs={"class":"pricefkey size"+str(size.pk)}))
             self.pricefkeylist.append(self[pricefkeyid])
--- sales/views.py
+++ sales/views.py
@@ -14,53 +14,76 @@ def shirtorderview(request, orderid):
     
     return render_to_response('sales/shirtorders/view.html', {'shirtorder':shirtorder})
     
-def shirtorderadd(request):
-    if request.method == 'GET':
+def shirtorderadd(request, orderid=None):
+    if request.method == 'GET': 
+        orderlines=[]   
+        if orderid != None:
+            editshirtorder = ShirtOrder.objects.get(pk=orderid)
+            editorderskus = ShirtOrderSKU.objects.filter(ShirtOrder=editshirtorder)
+            skustylegroups = {}
+            for sku in editorderskus:
+                group = (sku.ShirtPrice.ShirtStyle.id, sku.ShirtStyleVariation.id if sku.ShirtStyleVariation else None, sku.Color.id)
+                try:
+                    skustylegroups[group].append(sku)
+                except KeyError:
+                    skustylegroups[group] = [sku]
+            
+            prefixes = (i for i in xrange(1, len(skustylegroups)+1))
+            orderlines = [OrderLine(
+                            shirtstyleid=shirtstyleid,
+                            shirtstylevariationid=shirtstylevariationid,
+                            colorid=colorid,
+                            prefix=prefixes.next(),
+                            existingskus=skustylegroups[(shirtstyleid, shirtstylevariationid, colorid)]
+                          ) for shirtstyleid, shirtstylevariationid, colorid
+                            in skustylegroups.keys()]
+
         shirtstyles = ShirtStyle.objects.filter(Customer=None)
-        my_context = RequestContext(request, {'shirtorder':Order(),'shirtstyles':shirtstyles})
+        my_context = RequestContext(request, {'shirtorder':Order(instance=editshirtorder) if orderid != None else Order(),'shirtstyles':shirtstyles, "orderlines": orderlines})
         return render_to_response('sales/shirtorders/add.html',my_context)
+
     else:
-    #created variable to test validation
+        #created variable to test validation
         passedvalidation = True
         order = Order(request.POST)
-    
-    #if order is not valid, change validation variable
-    if not order.is_valid():
-        passedvalidation = False
         
-    #created separate orderlines from post data
-    orderlines = []
-    for i in xrange(1, int(request.POST['rows']) + 1):
-        orderlines.append(OrderLine(request.POST, prefix=i, shirtstyleid=request.POST[str(i) + "-shirtstyleid"]))
-    
-    #check each orderline for validity, change validation variable if any of them fail
-    for orderline in orderlines:
-        if not orderline.is_valid():
+        #if order is not valid, change validation variable
+        if not order.is_valid():
             passedvalidation = False
-    
-    #if any validation failed, send data back to page for revision
-    if passedvalidation == False:
-        #print orderlines[0]
-        shirtstyles = ShirtStyle.objects.filter(Customer=None)
-        my_context = RequestContext(request, {"orderlines": orderlines, "shirtorder": order, "shirtstyles":shirtstyles})
-        return render_to_response("sales/shirtorders/add.html", my_context)
-    
-    #if all validation passed, save order, then save orderlines
-    else:
-        shirtorder = ShirtOrder(Customer=order.cleaned_data['Customer'], CustomerAddress=order.cleaned_data['CustomerAddress'], PONumber=order.cleaned_data['PONumber'])
-        shirtorder.save()
+            
+        #created separate orderlines from post data
+        orderlines = []
+        for i in xrange(1, int(request.POST['rows']) + 1):
+            orderlines.append(OrderLine(request.POST, prefix=i, shirtstyleid=request.POST[str(i) + "-shirtstyleid"]))
+        
+        #check each orderline for validity, change validation variable if any of them fail
         for orderline in orderlines:
-            for s in xrange(1, orderline.cleaned_data["sizes"]):
-                if 'quantity'+str(s) in orderline.fields and orderline.cleaned_data['quantity'+str(s)] != None:
-                    ShirtOrderSKU( ShirtOrder=shirtorder
-                                 , ShirtPrice=ShirtPrice.objects.get(pk=orderline.cleaned_data['pricefkey'+str(s)])
-                                 , ShirtStyleVariation=None if orderline.cleaned_data['shirtstylevariationid']==None else ShirtStyleVariation.objects.get(pk=orderline.cleaned_data['shirtstylevariationid'])
-                                 , Color=orderline.cleaned_data['color']
-                                 , OrderQuantity=orderline.cleaned_data['quantity'+str(s)]
+            if not orderline.is_valid():
+                passedvalidation = False
+        
+        #if any validation failed, send data back to page for revision
+        if passedvalidation == False:
+            #print orderlines[0]
+            shirtstyles = ShirtStyle.objects.filter(Customer=None)
+            my_context = RequestContext(request, {"orderlines": orderlines, "shirtorder": order, "shirtstyles":shirtstyles})
+            return render_to_response("sales/shirtorders/add.html", my_context)
+        
+        #if all validation passed, save order, then save orderlines
+        else:
+            shirtorder = ShirtOrder(Customer=order.cleaned_data['Customer'], CustomerAddress=order.cleaned_data['CustomerAddress'], PONumber=order.cleaned_data['PONumber'])
+            shirtorder.save()
+            for orderline in orderlines:
+                for s in xrange(1, orderline.cleaned_data["sizes"]):
+                    if 'quantity'+str(s) in orderline.fields and orderline.cleaned_data['quantity'+str(s)] != None:
+                        ShirtOrderSKU( ShirtOrder=shirtorder
+                                     , ShirtPrice=ShirtPrice.objects.get(pk=orderline.cleaned_data['pricefkey'+str(s)])
+                                     , ShirtStyleVariation=None if orderline.cleaned_data['shirtstylevariationid']==None else ShirtStyleVariation.objects.get(pk=orderline.cleaned_data['shirtstylevariationid'])
+                                     , Color=orderline.cleaned_data['color']
+                                     , OrderQuantity=orderline.cleaned_data['quantity'+str(s)]
 
-                                 , Price=orderline.cleaned_data['price'+str(s)]
-                                 ).save()
-        return HttpResponseRedirect('/shirtorders/')
+                                     , Price=orderline.cleaned_data['price'+str(s)]
+                                     ).save()
+            return HttpResponseRedirect('/shirtorders/')
     
 def orderline(request):
     if "shirtstyleid" in request.GET:
