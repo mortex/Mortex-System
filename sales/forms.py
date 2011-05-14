bb910783d7af1c8c325b8d5c7eb6c114bb544893
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-05-14 18:56:21 -0400

    form submitting and validating, only need to get all extraneous data passing to form after validation failure

--- sales/forms.py
+++ sales/forms.py
@@ -1,10 +1,39 @@
+from django import forms
+from sales.models import ShirtSKUInventory, ShirtSize
 
-
-class ShirtSKUInventoryForm(forms.ModelForm):
+class CutSSIForm(forms.ModelForm):
+    'allows you to create transactions for new cut orders of a shirt SKU'
     class Meta:
         model = ShirtSKUInventory
         widgets = {
-            "Color": HiddenInput(),
-            "ShirtPrice": HiddenInput(),
-            "ShirtStyleVariation": HiddenInput(),
+            "Color": forms.HiddenInput(),
+            "ShirtPrice": forms.HiddenInput(),
+            "ShirtStyleVariation": forms.HiddenInput(),
         }
+        exclude = ("Date",)
+    Pieces = forms.IntegerField(required=False, min_value=0)
+    def __init__(self, *args, **kwargs):
+        super(CutSSIForm, self).__init__(*args, **kwargs)
+        if "instance" in kwargs:
+            self.shirtsize = kwargs["instance"].ShirtPrice.ShirtSize
+            self.cutorder = kwargs["instance"].CutOrder
+
+class NewCutSSIForm(CutSSIForm):
+    FormType = forms.CharField(initial='new', widget=forms.HiddenInput())
+    CutOrder = forms.CharField(required=False)
+
+class ExistingCutSSIForm(CutSSIForm):
+    'allows you to create transactions for existing cut orders of a shirt SKU'
+    class Meta:
+        model = ShirtSKUInventory
+        widgets = {
+            "CutOrder":forms.HiddenInput(),
+            "Color": forms.HiddenInput(),
+            "ShirtPrice": forms.HiddenInput(),
+            "ShirtStyleVariation": forms.HiddenInput(),
+        }
+        exclude = ("Date",)
+    FormType = forms.CharField(initial='existing', widget=forms.HiddenInput())
+    def __init__(self, *args, **kwargs):
+        self.totalpieces = kwargs.pop("total_pieces", None)
+        super(ExistingCutSSIForm, self).__init__(*args, **kwargs)
