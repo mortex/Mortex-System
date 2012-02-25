11fb08d56910984cb9cdd9282938f98ff16eb7eb
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2012-02-25 12:06:21 -0500

    added urls and templates for authentication app

--- sales/urls.py
+++ sales/urls.py
@@ -9,7 +9,16 @@ info_dict = {
 
 urlpatterns = patterns('',
     (r'^$', login_required(direct_to_template), {'template': 'sales/index.html'}),
+    
+    #authentication urls
     (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
+    (r'^changepassword/$', 'django.contrib.auth.views.password_change'),
+    (r'^changepassworddone/$', 'django.contrib.auth.views.password_change_done'),
+    (r'^resetpassword/$', 'django.contrib.auth.views.password_reset', {'is_admin_site':False}),
+    (r'^resetpassworddone/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^resetpasswordcomplete/$', 'django.contrib.auth.views.password_reset_complete'),
+    (r'^resetpasswordconfirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
     
     #inventory urls
     (r'^inventory/(?P<shirtstyleid>\d+)/(?P<variationid>\d+)/(?P<colorid>\d+)$', 'sales.views.manageinventory'),
--- settings.py
+++ settings.py
@@ -107,3 +107,12 @@ except ImportError:
     pass
     
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+#email stuff
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'no-reply@companyshopsmarket.coop'
+EMAIL_HOST_PASSWORD = 'L0calpe0ple42'
+EMAIL_PORT = 587
